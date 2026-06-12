e45b26c1c48bd309b12c060e8fa73253150c3025
anant707xINF <anantyadav864@gmail.com>
2026-06-12 18:06:12 +0530

    files uploaded

---
+++ day 1/operators.java
@@ -0,0 +1,33 @@
+class operators{
+    public static void main(String[] args) {
+        //ARITHMETIC OPERATORS//
+       /*  int a = 10 ;
+        int b = 5 ;
+        int c = a + b ;
+        System.out.println("result is : ");
+        System.out.println(c);   //15
+        System.out.println(a-b); //5
+        System.out.println(a*b); //50
+        System.out.println(a/b); //2
+        System.out.println(a%b); //0 
+    */
+
+      /*   int a = 2 ;
+        a+=2 ; // a = a + 2 ;
+        System.out.println(a);
+        a+=3; // 7 
+        System.out.println(a);
+        a/=2; //   a = a/2; // ans = 3 ;
+        System.out.println(a);
+        a-=3 ;
+        System.out.println(a); // ans = 0 ;
+        */
+
+        int i = 2 ;
+        i++ ; //  i = i + 1 ;
+        System.out.println(i);
+        i--; 
+        System.out.println(i);
+
+    }
+}
---
+++ day 1/typeconversion.java
@@ -0,0 +1,20 @@
+class typeconversion{
+    public static void main(String[] args) {
+      //  int a = 5 ;
+      //  byte b ;
+      //  b = (byte) a ;
+      //  System.out.println(b);
+       
+      /*  byte a = 32 ;
+        int b ;
+        b = a ;
+        System.out.println(b); 
+        */
+
+        long num1 = 342 ;
+        short num2 ;
+        num2 = (short) num1 ;
+        System.out.println(num2);
+
+    }
+}
